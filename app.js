8d8728b0b7576a45c15525505414ddb8265eca97
Patrick Coleman <colemanrpatrick@gmail.com>
2016-02-22 10:01:35 -0500

    first

---
+++ app.js
@@ -0,0 +1,320 @@
+////navbar///////
+
+$(document).ready(function(){
+$('li').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $(targetClass).addClass('active');
+ });
+});
+////almbum 1/////
+$(document).ready(function(){
+$('.pic1').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic1zoom').addClass('active');
+ })
+});
+///////////////////
+
+$(document).ready(function(){
+$('.pic1zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album1').addClass('active');
+ })
+});
+
+////////////////////
+$(document).ready(function(){
+$('.pic2').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic2zoom').addClass('active');
+ })
+});
+
+$(document).ready(function(){
+$('.pic2zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album1').addClass('active');
+ })
+});
+
+
+
+
+
+$(document).ready(function(){
+$('.pic3').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic3zoom').addClass('active');
+ })
+});
+
+
+$(document).ready(function(){
+$('.pic3zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album1').addClass('active');
+ })
+});
+
+
+
+
+
+////album2////
+
+$(document).ready(function(){
+$('.pic4').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic4zoom').addClass('active');
+ })
+});
+
+
+$(document).ready(function(){
+$('.pic4zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album2').addClass('active');
+ })
+});
+
+$(document).ready(function(){
+$('.pic5').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic5zoom').addClass('active');
+ })
+});
+$(document).ready(function(){
+$('.pic5zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album2').addClass('active');
+ })
+});
+
+$(document).ready(function(){
+$('.pic6').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic6zoom').addClass('active');
+ })
+});
+
+$(document).ready(function(){
+$('.pic6zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album2').addClass('active');
+ })
+});
+
+/////album 3/////////////////////////
+
+$(document).ready(function(){
+$('.pic7').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic7zoom').addClass('active');
+ });
+});
+$(document).ready(function(){
+$('.pic7zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album3').addClass('active');
+ });
+});
+
+
+$(document).ready(function(){
+$('.pic8').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic8zoom').addClass('active');
+ });
+});
+$(document).ready(function(){
+$('.pic8zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album3').addClass('active');
+ });
+});
+
+$(document).ready(function(){
+$('.pic9').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic9zoom').addClass('active');
+ });
+});
+
+$(document).ready(function(){
+$('.pic9zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album3').addClass('active');
+ });
+});
+
+
+
+
+
+///album4/////////////////////////
+
+$(document).ready(function(){
+$('.pic10').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic10zoom').addClass('active');
+ });
+});
+$(document).ready(function(){
+$('.pic10zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album4').addClass('active');
+ });
+});
+
+
+$(document).ready(function(){
+$('.pic11').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic11zoom').addClass('active');
+ });
+});
+
+$(document).ready(function(){
+$('.pic11zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album4').addClass('active');
+ });
+});
+
+
+$(document).ready(function(){
+$('.pic12').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic12zoom').addClass('active');
+ });
+});
+
+
+$(document).ready(function(){
+$('.pic12zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album4').addClass('active');
+ });
+});
+
+///////album5////////////
+$(document).ready(function(){
+$('.pic13').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic13zoom').addClass('active');
+ });
+});
+$(document).ready(function(){
+$('.pic13zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album5').addClass('active');
+ });
+});
+
+$(document).ready(function(){
+$('.pic14').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic14zoom').addClass('active');
+ });
+});
+$(document).ready(function(){
+$('.pic14zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album5').addClass('active');
+ });
+});
+
+
+$(document).ready(function(){
+$('.pic15').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic15zoom').addClass('active');
+ });
+});
+
+$(document).ready(function(){
+$('.pic15zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album5').addClass('active');
+ });
+});
+
+
+////////album6//////////
+$(document).ready(function(){
+$('.pic16').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic16zoom').addClass('active');
+ });
+});
+
+$(document).ready(function(){
+$('.pic16zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album6').addClass('active');
+ });
+});
+
+$(document).ready(function(){
+$('.pic17').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic17zoom').addClass('active');
+ });
+});
+$(document).ready(function(){
+$('.pic17zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album6').addClass('active');
+ });
+});
+
+
+$(document).ready(function(){
+$('.pic18').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.pic18zoom').addClass('active');
+ });
+});
+$(document).ready(function(){
+$('.pic18zoom').click(function(event){
+  var targetClass = "." + $(this).attr('rel');
+    $('.content').removeClass('active');
+    $('.album6').addClass('active');
+ });
+});
